a65ee75747732dd324ef690ea49df8d400c15c18
Parusharam <parusharam4888@gmail.com>
2024-09-10 19:11:47 +0530

    Home Page Responsive done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-
 import { Card } from 'react-bootstrap';
 import axios from 'axios';
 import "slick-carousel/slick/slick.css";
@@ -10,7 +9,8 @@ import './Home.css';
 
 const image_api = 'https://image.tmdb.org/t/p/w500/';
 
-export default function Home() {
+const MovieSlider = ({ title, movies }) => {
+  const navigate = useNavigate();
   const settings = {
     dots: true,
     infinite: false,
@@ -46,118 +46,73 @@ export default function Home() {
     ],
   };
 
+  return (
+    <div className="movie-category">
+      <h6>{title}</h6>
+      <div className="movies-slider-container">
+        <Slider {...settings} className="slider">
+          {movies.map((movie, index) => (
+            <Card
+              key={index}
+              onClick={() => navigate(`/movie/${movie.id}`, { state: movie })}
+              className="card"
+            >
+              <Card.Img
+                src={movie.poster_path ? image_api + movie.poster_path : 'placeholder_image_url'}
+                className="card-img"
+                alt={movie.title}
+              />
+              <Card.Title className="card-title">{movie.title}</Card.Title>
+            </Card>
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default function Home() {
   const [nowPlaying, setNowPlaying] = useState([]);
   const [popularMovies, setPopularMovies] = useState([]);
   const [topRated, setTopRated] = useState([]);
   const [upcomingMovies, setUpcomingMovies] = useState([]);
-  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchMovies = async (url, setState) => {
+    try {
+      const response = await axios.get(url);
+      setState(response.data.results);
+    } catch (error) {
+      setError('Failed to load data. Please try again later.');
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const movie_api = 'https://api.themoviedb.org/3/movie/now_playing?api_key=58ad96e97ac2915a7e028592171533fb';
-    axios.get(movie_api).then((response) => {
-      const movies = response.data.results;
-      setNowPlaying(movies);
-      console.log(movie_api)
-    });
+    const apiKey = '58ad96e97ac2915a7e028592171533fb';
+    fetchMovies(`https://api.themoviedb.org/3/movie/now_playing?api_key=${apiKey}`, setNowPlaying);
+    fetchMovies(`https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`, setPopularMovies);
+    fetchMovies(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`, setTopRated);
+    fetchMovies(`https://api.themoviedb.org/3/movie/upcoming?api_key=${apiKey}`, setUpcomingMovies);
   }, []);
 
-  useEffect(() => {
-    const popularMovies_api = 'https://api.themoviedb.org/3/movie/popular?api_key=58ad96e97ac2915a7e028592171533fb';
-    axios.get(popularMovies_api).then((response) => {
-      const movies = response.data.results;
-      setPopularMovies(movies);
-    });
-  }, []);
+  if (loading) {
+    return <div className="loading">Loading...</div>;
+  }
 
-  useEffect(() => {
-    const topRatedMovies_api = 'https://api.themoviedb.org/3/movie/top_rated?api_key=58ad96e97ac2915a7e028592171533fb';
-    axios.get(topRatedMovies_api).then((response) => {
-      const movies = response.data.results;
-      setTopRated(movies);
-    });
-  }, []);
-
-  useEffect(() => {
-    const upcomingMovies_api = 'https://api.themoviedb.org/3/movie/upcoming?api_key=58ad96e97ac2915a7e028592171533fb';
-    axios.get(upcomingMovies_api).then((response) => {
-      const movies = response.data.results;
-      setUpcomingMovies(movies);
-    });
-  }, []);
+  if (error) {
+    return <div className="error-message">{error}</div>;
+  }
 
   return (
-    <div className="home-container">    
-        
-          <div className="now-playing">
-            <h6>Now Playing</h6>
-            <div className="movies-slider-container">
-              <Slider {...settings} className='slider'>
-                {nowPlaying.map((movie, index) => (
-                  <Card
-                    key={index}
-                    onClick={() => navigate(`/movie/${movie.id}`, { state: movie })}
-                    className="card"
-                  >
-                    <Card.Img src={image_api + movie.poster_path} className="card-img" />
-                    <Card.Title className="card-title">{movie.title}</Card.Title>
-                  </Card>
-                ))}
-              </Slider>
-            </div>
-          </div>
-          <div className="popular-movies">
-            <h6>Popular Movies</h6>
-            <div className="movies-slider-container">
-              <Slider {...settings} className='slider'>
-                {popularMovies.map((movie, index) => (
-                  <Card
-                    key={index}
-                    onClick={() => navigate(`/movie/${movie.id}`, { state: movie })}
-                    className="card"
-                  >
-                    <Card.Img src={image_api + movie.poster_path} className="card-img" />
-                    <Card.Title className="card-title">{movie.title}</Card.Title>
-                  </Card>
-                ))}
-              </Slider>
-            </div>
-          </div>
-          <div className="top-rated">
-            <h6>Top Rated</h6>
-            <div className="movies-slider-container">
-              <Slider {...settings} className='slider'>
-                {topRated.map((movie, index) => (
-                  <Card
-                    key={index}
-                    onClick={() => navigate(`/movie/${movie.id}`, { state: movie })}
-                    className="card"
-                  >
-                    <Card.Img src={image_api + movie.poster_path} className="card-img" />
-                    <Card.Title className="card-title">{movie.title}</Card.Title>
-                  </Card>
-                ))}
-              </Slider>
-            </div>
-          </div>
-          <div className="upcoming-movies">
-            <h6>Upcoming Movies</h6>
-            <div className="movies-slider-container">
-              <Slider {...settings} className='slider'>
-                {upcomingMovies.map((movie, index) => (
-                  <Card
-                    key={index}
-                    onClick={() => navigate(`/movie/${movie.id}`, { state: movie })}
-                    className="card"
-                  >
-                    <Card.Img src={image_api + movie.poster_path} className="card-img" />
-                    <Card.Title className="card-title">{movie.title}</Card.Title>
-                  </Card>
-                ))}
-              </Slider>
-            </div>
-          </div>
-        
-      </div>
- 
+    <div className="home-container">
+      <MovieSlider title="Now Playing" movies={nowPlaying} />
+      <MovieSlider title="Popular Movies" movies={popularMovies} />
+      <MovieSlider title="Top Rated" movies={topRated} />
+      <MovieSlider title="Upcoming Movies" movies={upcomingMovies} />
+    </div>
   );
 }
