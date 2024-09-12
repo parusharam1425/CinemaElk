748564356ec9786d37371bf4e3221b1c6801ca49
Parusharam <parusharam4888@gmail.com>
2024-09-12 16:01:43 +0530

    completed

--- src/Components/Navbar/Sidebar.jsx
+++ src/Components/Navbar/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { AiFillHome } from 'react-icons/ai';
-import { FaCommentDots, FaUserCircle } from 'react-icons/fa';
+import { FaUserCircle } from 'react-icons/fa';
+import { BiSolidCameraMovie } from "react-icons/bi";
 import { useNavigate } from 'react-router-dom';
 
 
@@ -10,7 +11,8 @@ function Sidebar() {
     <div className='d-flex'>
          <div className="sidebar">
           <AiFillHome onClick={() => navigate('/')} className="sidebar-icons home-icon" />
-          <FaCommentDots onClick={() => navigate('/reviews')} className="sidebar-icons comment-icon" />
+          {/* <FaCommentDots onClick={() => navigate('/reviews')} className="sidebar-icons comment-icon" /> */}
+          <BiSolidCameraMovie onClick={() => navigate('/reviews')} className="sidebar-icons comment-icon" />
           <FaUserCircle onClick={() => navigate('/profile')} className="sidebar-icons user-icon" />
         </div>
         <hr className="vertical-divider" />
